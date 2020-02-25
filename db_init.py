c2b2404061ac07e353f6225c68c59120ef5dd023
sukepp <hanpulous@gmail.com>
2020-02-25 01:12:24 +0000

    1. Create the bike and weather class for databse. 2. Init the station table.  Changes to be committed: 	new file:   db_init.py 	modified:   model.py 	new file:   static/station.json

---
+++ db_init.py
@@ -0,0 +1,27 @@
+from model import session, Base, Station, engine
+import json
+
+# Drop all the tables
+Base.metadata.drop_all(engine, checkfirst=True)
+print("Deleted Tables")
+# Create the table based on class
+Base.metadata.create_all(engine)
+print("Created Tables")
+
+# Init the station data.
+contract_name = 'Dublin'
+data = json.load(open('static/station.json'))
+
+for station in data:
+    kwargs = {'id': station['number'],
+              'name': station['name'],
+              'address': station['address'],
+              'position_lat': station['position']['lat'],
+              'position_lng': station['position']['lng'],
+              'banking': station['banking'],
+              'bonus': station['bonus'],
+              'contract_name': 'Dublin'}
+    row_station = Station(**kwargs)
+    session.add(row_station)
+
+session.commit()
--- model.py
+++ model.py
@@ -1,10 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, create_engine, MetaData
+from sqlalchemy import Column, Integer, String, Float, Boolean, create_engine, ForeignKey, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Create the base class.
 Base = declarative_base()
-
 
 # Define the Station class
 class Station(Base):
@@ -20,17 +19,57 @@ class Station(Base):
     bonus = Column(Boolean, nullable=True)
     contract_name = Column(String(100), nullable=False)
 
+class Bike(Base):
+    # The name of table.
+    __tablename__ = "bike"
+    # The structure of the table.
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    station_id = Column(Integer, ForeignKey('station.id'))
+    status = Column(Boolean, nullable=False)
+    bike_stands = Column(Integer, nullable=False)
+    available_bike_stands = Column(Integer, nullable=False)
+    available_bikes = Column(Integer, nullable=False)
+    last_update = Column(DateTime, nullable=False)
+
+class Weather(Base):
+    # The name of table.
+    __tablename__ = "weather"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    # The structure of the table.
+    coord_lon = Column(Float)
+    coord_lat = Column(Float)
+    weather_id = Column(Integer)
+    weather_main = Column(String(45))
+    weather_description = Column(String(45))
+    weather_icon = Column(String(10))
+    base = Column(String(45))
+    main_temp = Column(Float)
+    main_feels_like = Column(Float)
+    main_temp_min = Column(Float)
+    main_temp_max = Column(Float)
+    main_pressure = Column(Integer)
+    main_humidity = Column(Integer)
+    visibility = Column(Integer)
+    wind_speed = Column(Float)
+    wind_deg = Column(Integer)
+    clouds_all = Column(Integer)
+    dt = Column(DateTime)
+    sys_type = Column(Integer)
+    sys_id = Column(Integer)
+    sys_message = Column(Float)
+    sys_country = Column(String(10))
+    sys_sunrise = Column(DateTime)
+    sys_sunset = Column(DateTime)
+    timezone = Column(Integer)
+    city_id = Column(Integer)
+    name = Column(String(45))
+    cod = Column(Integer)
+
 # Init the database connection.
 # For the local database test.
-#engine = create_engine('mysql+pymysql://root:hanpeisong@localhost:3306/dbbikes')
+# engine = create_engine('mysql+pymysql://root:hanpeisong@localhost:3306/dbbikes')
 # For the RDS
 engine = create_engine('mysql+pymysql://root:hanpeisong@dbbikes.cw9hkqmrhrqy.eu-west-1.rds.amazonaws.com:3306/dbbikes')
-
-# Drop all the tables
-Base.metadata.drop_all(engine, checkfirst=True)
-
-# Create the table based on class
-Base.metadata.create_all(engine)
 
 # Create the type of DBSession.
 DBSession = sessionmaker(bind=engine)
@@ -38,15 +77,3 @@ DBSession = sessionmaker(bind=engine)
 # Create the session object.
 session = DBSession()
 
-# TODO: Add the new tuples into station table.
-#kwargs = {'id': 0,
-#          'contract_name': 'test',
-#          'name': 'test',
-#          'address': 'test',
-#          'position_lat': 0.0,
-#          'position_lng': 0.0,
-#          'banking': False,
-#          'bonus': False}
-#station = Station(**kwargs)
-#session.add(station)
-#session.commit()
