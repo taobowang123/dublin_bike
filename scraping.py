cea119f85525f00ff085543c04e6f61ae0d5d946
BOTAO WANG <841226510@qq.com>
2020-02-25 20:10:56 +0000

    add time convert_to_standard function to change the timestamp

--- scraping.py
+++ scraping.py
@@ -2,7 +2,9 @@ import requests #to get the http request
 import json #to handle with json type file
 from pprint import pprint #to print pretty
 import threading #to repeatedly get the data from the web
+import time #to change the timestamp to normal time
 from model import session,Station,Bike,Weather
+
 
 def scraping_bikes(api_key,city):
     """Function to scrape Dublin Bikes using API
@@ -38,14 +40,17 @@ def stations_to_db(data):
     stations=json.loads(data)
     print(type(stations),len(stations))
     for station in stations:
+        timestamp=station.get('last_update')
+        time_standard=timestamp_convert(timestamp)
         print(station)
         kwargs={'station_id': int(station.get('number')),
               'bike_stands': int(station.get('bike_stands')),
               'available_bike_stands':int(station.get('available_bike_stands')),
               'available_bikes':int(station.get('available_bikes')),
               'status':station.get('status'),
-              'last_update':station.get('last_update')
+              'last_update':time_standard
                 }
+
         row_bike = Bike(**kwargs)
         session.add(row_bike)
     session.commit()
@@ -88,6 +93,14 @@ def stations_to_db(data):
  #    return
 
 # this class is used for loop calling the function,rewrite the run method
+
+def timestamp_convert(timestamp):
+    if len(str(timestamp))==13:
+        timeStamp=str(timestamp)[:-3]
+        localTime = time.localtime(int(timeStamp))
+        strTime = time.strftime("%Y-%m-%d %H:%M:%S", localTime)
+        return strTime
+
 class RepeatingTimer(threading.Timer):
     def run(self):
         while not self.finished.is_set():
@@ -98,7 +111,7 @@ api_key_bikes="0bef7f2fab41ef8a396b1e3ab0c929ac8138e3e1"
 city="DUBLIN"
 api_key_weather=""
 # setting the scraper on a timer - every five second
-bike = RepeatingTimer(5.0, scraping_bikes, args=[api_key_bikes,city])
-weather = RepeatingTimer(5.0, api_key_weather, args=[api_key_bikes,city])
+bike = RepeatingTimer(300.0, scraping_bikes, args=[api_key_bikes,city])
+weather = RepeatingTimer(300.0, api_key_weather, args=[api_key_bikes,city])
 bike.start()
 # weather.start()
